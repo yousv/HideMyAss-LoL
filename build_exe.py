cc25ee19fae69f3ebb9bbc2620de3dd2ef082254
Yousv <68682169+yousv@users.noreply.github.com>
2024-12-26 23:51:35 +0200

    Update build_exe.py

--- build_exe.py
+++ build_exe.py
@@ -1,16 +1,15 @@
 import PyInstaller.__main__
 import os
 
-# Add the icon path
 icon_path = os.path.abspath("app.ico")
 
 PyInstaller.__main__.run([
-    'status_manager.py',
+    'hma-lol.py',
     '--onefile',
     '--noconsole',
     '--uac-admin',
     '--manifest', 'admin_manifest.xml',
-    '--name', 'LoLStatusManager',
+    '--name', 'HideMyAss LoL',
     '--icon', icon_path,
     '--add-data', f'{icon_path};.'
 ])
